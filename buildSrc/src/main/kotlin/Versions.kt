3c417674ab9729b541790e8243d1c9a4296fec4b
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-19 14:48:54 +0700

    update to kotlin 1.3.60 (beta coroutines, permissions)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,10 +2,10 @@ object Versions {
     object Android {
         const val compileSdk = 28
         const val targetSdk = 28
-        const val minSdk = 21
+        const val minSdk = 16
     }
 
-    const val kotlin = "1.3.50"
+    const val kotlin = "1.3.60"
 
     object Plugins {
         const val kotlin = Versions.kotlin
@@ -13,15 +13,15 @@ object Versions {
 
     object Libs {
         object Android {
-            const val appCompat = "1.0.2"
+            const val appCompat = "1.1.0"
             const val exifInterface = "1.0.0"
             const val mediaFilePicker = "1.8"
         }
 
         object MultiPlatform {
-            const val coroutines = "1.3.0"
-            const val mokoPermissions = "0.1.0"
-            const val mokoMedia = "0.1.0"
+            const val coroutines = "1.3.2-1.3.60-eap-76"
+            const val mokoPermissions = "0.3.0-dev-1"
+            const val mokoMedia = "0.2.0-dev-1"
         }
     }
 }
