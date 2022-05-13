74c70915e93d43152c99d67be9ead1ffd8c81536
mdubkov <mdubkov@icerockdev.com>
2022-05-13 14:15:42 +0700

    up version & add apple silicone support

--- media-build-logic/build.gradle.kts
+++ media-build-logic/build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    api("dev.icerock:mobile-multiplatform:0.12.0")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
-    api("com.android.tools.build:gradle:4.2.1")
-    api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0")
+    api("dev.icerock:mobile-multiplatform:0.14.1")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+    api("com.android.tools.build:gradle:7.0.1")
+    api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.1")
 }
